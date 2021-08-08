92e9e4f2346dad78c6c3a7f7468994b60aba8f83
Vikas Kumar <vk.pratap98@gmail.com>
2021-08-08 07:50:22 +0530

    added delete button and download button functionality in gallery

--- dbScript.js
+++ dbScript.js
@@ -32,7 +32,7 @@ function saveMedia(media){
 }
 
 function viewMedia(){
-    console.log("hi");
+
     if(!database){
         return;
     }
@@ -55,17 +55,31 @@ function viewMedia(){
             mediaCard.innerHTML = `<div class="actual-media"></div>
             <div class="media-buttons">
                 <button class="media-download">Download</button>
-                <button class="media-delete">Delete</button>
+                <button class="media-delete" data-mId="${cursor.value.mId}">Delete</button>
             </div>`
 
             let data = cursor.value.mediaData;
+            let key = cursor.value.mId;
 
             let actualMediaDiv = mediaCard.querySelector('.actual-media');
-            
+            let downloadBtn = mediaCard.querySelector(".media-download");
+            let deleteBtn = mediaCard.querySelector(".media-delete");
+
+            deleteBtn.addEventListener("click", function(ev){
+                let mId = ev.currentTarget.getAttribute("data-mId");
+                mId = Number(mId);
+                deleteMedia(mId)
+                ev.currentTarget.parentElement.parentElement.remove();
+            })
+
             if(typeof data == "string"){
                 let image = document.createElement("img");
                 image.src = data;
                 actualMediaDiv.append(image);
+
+                downloadBtn.addEventListener("click", function(){
+                    downloadMedia(data, "image");
+                })
             }
             else if(typeof data == "object"){
                 let video = document.createElement('video');
@@ -76,10 +90,36 @@ function viewMedia(){
                 let url = URL.createObjectURL(data);
                 video.src = url;
                 actualMediaDiv.append(video);
+
+                downloadBtn.addEventListener("click", function(){
+                    downloadMedia(url, "video");
+                })
             }
 
             galleryContainer.append(mediaCard);
             cursor.continue();
         }
-    })
+    });
+}
+
+function downloadMedia(url, type){
+    let anchor = document.createElement('a');
+    anchor.href = url;
+
+    if(type == "image"){
+        anchor.download = "image.png"
+    }
+    else{
+        anchor.download = "video.mp4"
+    }
+
+    anchor.click();
+
+    anchor.remove();
+}
+
+function deleteMedia(mId){
+    let txn = database.transaction("media", "readwrite");
+    let mediaObjectStore = txn.objectStore("media");
+    mediaObjectStore.delete(mId);
 }
